9c92db160e654a13471272818ea3327a6fd0994d
Blake Erickson <o.blakeerickson@gmail.com>
2017-04-28 07:02:53 -0600

    add support for lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,36 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+    render json: @lists, status: :ok
+  end
+
+  def create
+    @list = List.create!(list_params)
+    render json: @list, status: :ok
+  end
+
+  def show
+    @list = List.find(params[:id])
+    render json: @list, status: :ok
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    render json: @list, status: :ok
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    render status: :ok
+  end
+
+  private
+
+  def list_params
+    params.permit(:title)
+  end
+
+end
